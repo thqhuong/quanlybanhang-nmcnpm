752a2a4ab363c8c3e7bdce7523e6243ef3aefac1
dhuyy-06 <huyqt3062006@gmail.com>
2026-05-23 12:27:24 +0700

    - Resolved issues #6, #7, #9, #10 - Implement logic to POS screen

--- Database/DatabaseConfiguration.cs
+++ Database/DatabaseConfiguration.cs
@@ -1,28 +1,35 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using System.Configuration;
 
 namespace quanlybanhang_nmcnpm.Database;
 
 public static class DatabaseConfiguration
 {
+    public const string ConnectionStringEnvironmentVariable = "QLBH_CONNECTION_STRING";
+    public const string DefaultConnectionString =
+        "Server=localhost;Database=QuanLyBanHang;Trusted_Connection=True;Encrypt=True;TrustServerCertificate=True;";
+
     public static IServiceCollection AddDatabaseServices(this IServiceCollection services)
     {
-        var connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"]?.ConnectionString 
-            ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found in App.config");
+        var connectionString = GetConnectionString();
 
-        services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(connectionString));
+        services.AddDbContext<ApplicationDbContext>(
+            options => options.UseSqlServer(connectionString),
+            ServiceLifetime.Transient);
 
         return services;
     }
 
+    public static string GetConnectionString()
+    {
+        return Environment.GetEnvironmentVariable(ConnectionStringEnvironmentVariable)
+            ?? DefaultConnectionString;
+    }
+
     public static async Task InitializeDatabaseAsync(this IServiceProvider serviceProvider)
     {
-        using (var scope = serviceProvider.CreateScope())
-        {
-            var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-            await dbContext.Database.MigrateAsync();
-        }
+        using var scope = serviceProvider.CreateScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        await dbContext.Database.MigrateAsync();
     }
 }
